94671b926800eaa2c2f6e48f3df548488072b71d
Fernando Cavalcanti(DK) <comododk@gmail.com>
2022-06-04 19:45:09 -0300

    Feat: start project

---
+++ vortex-comics/src/App.tsx
@@ -0,0 +1,15 @@
+import { GlobalStyle } from "./styles/global.jsx";
+
+function App() {
+
+  return (
+    <>
+      <GlobalStyle />
+      <div>
+        test page
+      </div>
+    </>
+  )
+}
+
+export default App

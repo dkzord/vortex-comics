217dec43b70683f1b6267905f7769b862f64a1ac
Fernando Cavalcanti(DK) <comododk@gmail.com>
2022-06-05 12:15:05 -0300

    Feat: test request API

--- vortex-comics/src/pages/HomePage/HomePage.tsx
+++ vortex-comics/src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import md5 from 'md5';
+
+const baseURL = 'http://gateway.marvel.com/v1/public/comics?';
+
+const publicKey = 'd4eca758697ee5df61dc197b27ff1f99';
+const privateKey = '64af5fb66d4dbf53e96f3d07041073af6b290260';
+
+const time = Number(new Date());
+
+const hash = md5(time + privateKey + publicKey);
 
 const HomePage = () => {
+  useEffect(() => {
+    axios.get(`http://gateway.marvel.com/v1/public/comics?ts=${time}&apikey=${publicKey}&hash=${hash}`)
+    .then(resp => console.log(resp))
+    .catch(err => console.log(err))
+  }, [])
+
   return (
     <h1>
-      Home Page
+      Marvel
     </h1>
   )
 }

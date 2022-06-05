63257fba6b794839c7d85c7c08359e46f9eba667
Fernando Cavalcanti(DK) <comododk@gmail.com>
2022-06-04 21:20:31 -0300

    Feat: create helmet

--- vortex-comics/src/App.tsx
+++ vortex-comics/src/App.tsx
@@ -1,14 +1,26 @@
-import { GlobalStyle } from "./styles/global.jsx";
+import React from "react";
+import { useState } from "react";
+import { Helmet } from "react-helmet";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { darkTheme, lightTheme } from "./styles/variables";
+
+export const ThemeContext = React.createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const themeStyle = theme === 'light' ? lightTheme : darkTheme;
 
   return (
-    <>
+    <ThemeProvider theme={themeStyle} >
       <GlobalStyle />
-      <div>
-        test page
-      </div>
-    </>
+      <Helmet>
+        <title>Vortex Comics</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet" />
+      </Helmet>
+    </ThemeProvider>
   )
 }
 
--- vortex-comics/src/styles/global.ts
+++ vortex-comics/src/styles/global.ts
@@ -1,8 +1,10 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   :root {
     --background: #FCFCFC;
+
+
     --color-green: #9CDD6E;
     --color-green-light: #9CDD6E;
     --color-yellow-light: #FFF66B;
@@ -15,13 +17,15 @@ export const GlobalStyle = createGlobalStyle`
     --color-gray: #858585;
     --color-white: #FFF;
   }
-  * {
+  
+  *, *::before, *::after {
     margin: 0;
     padding: 0;
     outline: none;
     box-sizing: border-box;
-    font-family: 'Poppins', sans-serif;
   }
+
+
   //Tamanho de font 16px (para desktop) por padrão
   html{
     @media(max-width: 1080px){
@@ -31,22 +35,30 @@ export const GlobalStyle = createGlobalStyle`
       font-sizing: 87.5%; //14px
     }
   }
+
   body {
-    background: var(--background);
+    background: ${({ theme }) => theme.bg2};
+    color: ${(theme) => theme.text};
+    letter-spacing: 0.6px;
+
     -webkit-font-smoothing: antialiased;
   }
+
   body, textarea, input, button {
     font-family: 'Poppins', sans-serif;
     font-weight: 400;
   }
+
   h1, h2, h3, h4, h5, h6, strong {
     font-weight: 600;
   }
+
   button {
     cursor: pointer;
   }
+
   [disabled] {
     opacity: 0.6;
     cursor: not-allowed;
   }
-`
+`;
